83244c1a45c7b945b5e4dff9f0f71f2265a999ef
Azarhel <azarhel.moriarty@gmail.com>
2024-04-06 14:21:02 -0600

    images

--- src/App.js
+++ src/App.js
@@ -1,51 +1,45 @@
+// Importa React y las dependencias necesarias
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Asegúrate de que Bootstrap esté instalado
+import './App.css'; // Estilos generales de tu App
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Componentes de react-router-dom
+import NavigationBar from './Navegacion/NavigationBar'; // Tu componente de barra de navegación
+import Home from './Paginas/Home'; // Componente de la página de inicio
+import About from './Paginas/About'; // Componente de la página de acerca de
 
-// Imágenes para el carrusel
-import image1 from './path-to-your-image1.jpg';
-import image2 from './path-to-your-image2.jpg';
-import image3 from './path-to-your-image3.jpg';
+// Importa las imágenes desde la carpeta src/images
+import image1 from './images/image1.jpg';
+import image2 from './images/image2.jpg';
+import image3 from './images/image3.jpg';
 
-function Home() {
+function App() {
   return (
     <div>
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={image1}
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Descripción de la primera imagen.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={image2}
-            alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Descripción de la segunda imagen.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={image3}
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Descripción de la tercera imagen.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <BrowserRouter>
+        <NavigationBar /> {/* Muestra tu barra de navegación en todas las páginas */}
+        <Routes>
+          {/* Define tus rutas y los componentes que deben renderizarse */}
+          <Route path='/' element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+      {/* Carrusel de Bootstrap con tus imágenes */}
+      <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={image1} className="d-block w-100" alt="Imagen 1" />
+          </div>
+          <div className="carousel-item">
+            <img src={image2} className="d-block w-100" alt="Imagen 2" />
+          </div>
+          <div className="carousel-item">
+            <img src={image3} className="d-block w-100" alt="Imagen 3" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Home;
+export default App;
+
