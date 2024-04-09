860e6e9eef6fa724cadccfdb279f2ad5f8a6ef68
Azarhel <azarhel.moriarty@gmail.com>
2024-04-09 16:38:21 -0600

    Tamaño

--- src/components/ContactoForm.js
+++ src/components/ContactoForm.js
@@ -22,47 +22,47 @@ const ContactoForm = () => {
     <div id='acerca de nosotros' className="h-screen flex items-center justify-center">
       <div className="max-w-lg mx-auto">
         <h2 className="text-3xl font-bold mb-4 text-white">Contacto</h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div>
-            <label htmlFor="nombre" className="block text-sm font-medium text-white">Nombre:</label>
-            <input
-              type="text"
-              id="nombre"
-              name="nombre"
-              value={nombre}
-              onChange={(e) => setNombre(e.target.value)}
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="correo" className="block text-sm font-medium text-white">Correo:</label>
-            <input
-              type="email"
-              id="correo"
-              name="correo"
-              value={correo}
-              onChange={(e) => setCorreo(e.target.value)}
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="mensaje" className="block text-sm font-medium text-white">Mensaje:</label>
-            <textarea
-              id="mensaje"
-              name="mensaje"
-              rows="4"
-              value={mensaje}
-              onChange={(e) => setMensaje(e.target.value)}
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
-              required
-            />
-          </div>
-          <button type="submit" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700">
-            Enviar
-          </button>
-        </form>
+<form onSubmit={handleSubmit} className="space-y-4">
+  <div>
+    <label htmlFor="nombre" className="block text-sm font-medium text-white">Nombre:</label>
+    <input
+      type="text"
+      id="nombre"
+      name="nombre"
+      value={nombre}
+      onChange={(e) => setNombre(e.target.value)}
+      className="mt-1 block w-full px-3 py-2 rounded-md border-gray-300 shadow-sm"
+      required
+    />
+  </div>
+  <div>
+    <label htmlFor="correo" className="block text-sm font-medium text-white">Correo:</label>
+    <input
+      type="email"
+      id="correo"
+      name="correo"
+      value={correo}
+      onChange={(e) => setCorreo(e.target.value)}
+      className="mt-1 block w-full px-3 py-2 rounded-md border-gray-300 shadow-sm"
+      required
+    />
+  </div>
+  <div>
+    <label htmlFor="mensaje" className="block text-sm font-medium text-white">Mensaje:</label>
+    <textarea
+      id="mensaje"
+      name="mensaje"
+      rows="4"
+      value={mensaje}
+      onChange={(e) => setMensaje(e.target.value)}
+      className="mt-1 block w-full px-3 py-2 rounded-md border-gray-300 shadow-sm"
+      required
+    />
+  </div>
+  <button type="submit" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700">
+    Enviar
+  </button>
+</form>
       </div>
     </div>
   );
