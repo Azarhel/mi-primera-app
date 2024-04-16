e9cc47d86a8da67c70d49d6383b4d558271ba2c5
Azarhel <azarhel.moriarty@gmail.com>
2024-04-15 21:25:17 -0600

    about

--- src/Paginas/About.js
+++ src/Paginas/About.js
@@ -3,8 +3,6 @@ import React from 'react';
 const About = () => {
   return (
     <div id='about' >
-      <h2>Acerca de nosotros</h2>
-      <p>¡Descubre más sobre nuestra empresa aquí!</p>
     </div>
   );
 };
